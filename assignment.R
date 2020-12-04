829fb4a3cf63afcf14ab23fd7393381e049b3915
UweDraeger <uwe.draeger@gmx.net>
2020-12-04 13:55:48 +0100

    added train_test flag to X_train, X_test

--- assignment.R
+++ assignment.R
@@ -1,10 +1,9 @@
 
 
+
 library(tidyverse)
 
-setwd(
-        "~/Coursera/ DataCleaning"
-)
+setwd("~/Coursera/ DataCleaning")
 
 # read activity labels
 activity_labels <-
@@ -23,67 +22,59 @@ feature_labels <-
 
 # read subject_train, subject_test - Do I need those as analysis focused on mean and std?
 subj_train <-
-        read_delim(
-                paste0(file.path(getwd(), "train", "subject_train.txt")),
-                delim = " ",
-                col_names = FALSE
-        )
+        read_delim(paste0(file.path(getwd(), "train", "subject_train.txt")),
+                   delim = " ",
+                   col_names = FALSE)
 subj_test <-
-        read_delim(
-                paste0(file.path(getwd(), "test", "subject_test.txt")),
-                delim = " ",
-                col_names = FALSE
-        )
+        read_delim(paste0(file.path(getwd(), "test", "subject_test.txt")),
+                   delim = " ",
+                   col_names = FALSE)
 
 # read y_train, y_test
 # create surrogate keys, then join the activity labels
 y_train <-
-        read_delim(
-                paste0(file.path(getwd(), "train", "y_train.txt")),
-                delim = " ",
-                col_names = FALSE
-        )
-y_train <- rowid_to_column(y_train, var = "y_id") %>% 
+        read_delim(paste0(file.path(getwd(), "train", "y_train.txt")),
+                   delim = " ",
+                   col_names = FALSE)
+y_train <- rowid_to_column(y_train, var = "y_id") %>%
         mutate(activity = activity_labels$activity[match(X1, activity_labels$activity_id)])
 y_test <-
-        read_delim(
-                paste0(file.path(getwd(), "test", "y_test.txt")),
-                delim = " ",
-                col_names = FALSE
-        )
-y_test <- rowid_to_column(y_test, var = "y_id") %>% 
+        read_delim(paste0(file.path(getwd(), "test", "y_test.txt")),
+                   delim = " ",
+                   col_names = FALSE)
+y_test <- rowid_to_column(y_test, var = "y_id") %>%
         mutate(activity = activity_labels$activity[match(X1, activity_labels$activity_id)])
 
 # read X_train, X_test - Do I need to care for NA's - problems(X-test)?
-# assumes same order when extracting column names of feature, which seems 
-# logical 
+# assumes same order when extracting column names of feature, which seems
+# logical
 X_train <-
-        read_delim(
-                paste0(file.path(getwd(), "train", "X_train.txt")),
-                delim = " ",
-                col_names = t(feature_labels[2])
-        ) %>% 
-        select(any_of(grep("-mean()", colnames(X_train))), any_of(grep("-std()", colnames(X_train))) )
+        read_delim(paste0(file.path(getwd(), "train", "X_train.txt")),
+                   delim = " ",
+                   col_names = t(feature_labels[2]))
+X_train <- X_train %>%
+        select(any_of(grep("-mean()", colnames(X_train))), any_of(grep("-std()", colnames(X_train)))) %>%
+        mutate(train_test = "train")
 X_test <-
-        read_delim(
-                paste0(file.path(getwd(), "test", "X_test.txt")),
-                delim = " ",
-                col_names = t(feature_labels[2])
-        ) %>% 
-        select(any_of(grep("-mean()", colnames(X_test))), any_of(grep("-std()", colnames(X_test))) )
+        read_delim(paste0(file.path(getwd(), "test", "X_test.txt")),
+                   delim = " ",
+                   col_names = t(feature_labels[2]))
+X_test <-  X_test %>%
+        select(any_of(grep("-mean()", colnames(X_test))), any_of(grep("-std()", colnames(X_test)))) %>%
+        mutate(train_test = "test")
 
-body_acc_x_train <- 
-        read_delim(
-                paste0(file.path(getwd(), "train", "Inertial Signals", "body_acc_x_train.txt")),
-                delim = " ",
-                col_names = FALSE
-        )
-body_acc_x_test <- 
-        read_delim(
-                paste0(file.path(getwd(), "test", "Inertial Signals", "body_acc_x_test.txt")),
-                delim = " ",
-                col_names = FALSE
-        )
+body_acc_x_train <-
+        read_delim(paste0(
+                file.path(getwd(), "train", "Inertial Signals", "body_acc_x_train.txt")
+        ),
+        delim = " ",
+        col_names = FALSE)
+body_acc_x_test <-
+        read_delim(paste0(
+                file.path(getwd(), "test", "Inertial Signals", "body_acc_x_test.txt")
+        ),
+        delim = " ",
+        col_names = FALSE)
 
 
 # You should create one R script called run_analysis.R that does the following.
